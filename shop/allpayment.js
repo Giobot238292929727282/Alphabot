4aca8467a52713bc13434c89c0f0f0e3a69e5b93
Giobot238292929727282 <96403857+Giobot238292929727282@users.noreply.github.com>
2022-05-21 12:30:05 +0700

    Add files via upload

---
+++ shop/allpayment.js
@@ -0,0 +1,34 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY ZEEONE OFC
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ BELUM ADA
+2. BCA 
+	ㅁ BELUM ADA
+3. BNI 
+	ㅁ BELUM ADA
+4. PERMATA
+	ㅁ BELUM ADA
+	
+*💰 E-MONEY*
+
+1. PULSA
+	ㅁ 0822 6971 8172
+2. OVO
+	ㅁ 0822 6971 8172
+3. DANA
+	ㅁ0822 6971 8172
+	
+Semuanya atas nama NTAH
+
+
+Tidak tersedia
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
---
+++ shop/donasi.js
@@ -0,0 +1,19 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+	
+*💰 E-MONEY*
+
+1. PULSA
+	ㅁ 0822 6971 8172
+2. OVO
+	ㅁ 0822 6971 8172
+3. DANA
+	ㅁ 0822 6971 8172
+	
+Semuanya atas nama BELUM ADA
+`
+	}
+
+exports.donasibot = donasibot
+ 
